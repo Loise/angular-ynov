923a365d31539089964f725e15c8195bac55f9f3
Myuuu <myuuu@live.fr>
2022-02-20 17:05:29 +0100

    Add custom props

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product-card',
@@ -6,34 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
-  title: string;
-  description: string;
-  imageUrl: string;
-  price: number;
-  likes: number;
-  isLiked: boolean;
+  @Input() myProduct: Product;
   constructor() { }
 
   ngOnInit() {
-    this.title = "Peluche Harry Potter",
-    this.description = "Peluche certifiée OEKO-TEX, coton bio issue du commerce équitable, taille 12 cm ",
-    this.imageUrl = "https://media.auchan.fr/42f06d04-cd4f-4cf0-8d86-48c56c856e31_512x512/B2CD/",
-    this.price = 2300
-    this.likes = 0;
-    this.isLiked = false;
+    this.myProduct = new Product("Peluche Harry Potter",
+      "Peluche certifiée OEKO-TEX, coton bio issue du commerce équitable, taille 12 cm ",
+      "https://media.auchan.fr/42f06d04-cd4f-4cf0-8d86-48c56c856e31_512x512/B2CD/",
+      2300,
+      0,
+      false);
   }
 
   onLike() {
-    if(this.isLiked) {
-      this.likes--;
+    if (this.myProduct.isLiked) {
+      this.myProduct.likes--;
     } else {
-      this.likes++;
+      this.myProduct.likes++;
     }
-    this.isLiked = !this.isLiked;
+    this.myProduct.isLiked = !this.myProduct.isLiked;
   }
-
-  /**
-   * onRemoveLike ?
-   */
-
 }
