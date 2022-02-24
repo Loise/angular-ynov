ee8e47dae48d2240207ba3e6edc170816ea33cf0
Myuuu <myuuu@live.fr>
2022-02-24 19:09:04 +0100

    correction custom properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  product!: Product;
+
+  ngOnInit() {
+    this.product = new Product(
+      "Peluche Harry Potter",
+      "Peluche certifiée OEKO-TEX, coton bio issue du commerce équitable, taille 12 cm ",
+      "https://media.auchan.fr/42f06d04-cd4f-4cf0-8d86-48c56c856e31_512x512/B2CD/",
+      2300,
+      0,
+      false,
+      ['XL']
+    );
+  }
 }
+
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -6,21 +6,11 @@ import { Product } from '../models/product.model';
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent implements OnInit {
-  @Input() myProduct: Product;
-  constructor() { }
-
-  ngOnInit() {
-    this.myProduct = new Product("Peluche Harry Potter",
-      "Peluche certifiée OEKO-TEX, coton bio issue du commerce équitable, taille 12 cm ",
-      "https://media.auchan.fr/42f06d04-cd4f-4cf0-8d86-48c56c856e31_512x512/B2CD/",
-      2300,
-      0,
-      false);
-  }
+export class ProductCardComponent {
+  @Input() myProduct!: Product;
 
   onLike() {
-    if (this.myProduct.isLiked) {
+    if(this.myProduct.isLiked) {
       this.myProduct.likes--;
     } else {
       this.myProduct.likes++;
