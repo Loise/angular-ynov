358a26c57f889c8faf66e76ce41794e19f37a8ea
Myuuu <myuuu@live.fr>
2022-03-01 23:08:24 +0100

    Add args to custom pipe

--- src/app/pipes/product.pipe.ts
+++ src/app/pipes/product.pipe.ts
@@ -3,7 +3,11 @@ import { Product } from '../models/product.model';
 
 @Pipe({ name: 'sortByDate' })
 export class SortByDatePipe implements PipeTransform {
-  transform(products: Product[]) {
-    return products.sort((a,b) => { return b.createdDate.getTime() - a.createdDate.getTime()} );
-  }
+    transform(products: Product[], order?: any) {
+        let desc = !(order && order === 'asc');
+        return products.sort((a, b) => {
+            if (desc) return b.createdDate.getTime() - a.createdDate.getTime()
+            else return a.createdDate.getTime() - b.createdDate.getTime()
+        });
+    }
 }
