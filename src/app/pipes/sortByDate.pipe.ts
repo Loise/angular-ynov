2a28570b8d9f920d6bcc456fac6b18580861f148
Myuuu <myuuu@live.fr>
2022-03-10 21:44:43 +0100

    fix sortByDate pipe when observable is not defined

--- src/app/pipes/sortByDate.pipe.ts
+++ src/app/pipes/sortByDate.pipe.ts
@@ -5,6 +5,7 @@ import { Product } from '../models/product.model';
 export class SortByDatePipe implements PipeTransform {
     transform(products: Product[], order?: any) {
         let desc = !(order && order === 'asc');
+        if(!products) return products;
         return products.sort((a, b) => {
             if (desc) return new Date(b.createdDate).getTime() - new Date(a.createdDate).getTime()
             else return new Date(a.createdDate).getTime() - new Date(b.createdDate).getTime()
